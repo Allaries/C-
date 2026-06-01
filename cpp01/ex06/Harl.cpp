46afe879535c96acc306a66e65e0b9492177fea8
rerichar <rerichar@f6r8s16.paris.42.school>
2026-06-01 11:56:42 +0200

    cpp01 FINI

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -11,17 +11,20 @@ Harl::~Harl()
 
 void    Harl::debug(void)
 {
-    std::cout << " love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
+    this->info();
 }
 
 void    Harl::info(void)
 {
     std::cout << "I cannot believe adding extra bacon costs more money. You didnt put enough bacon in my burger! If you did, I wouldnt be asking for more!\n";
+    this->warning();
 }
 
 void    Harl::warning(void)
 {
     std::cout << "think I deserve to have some extra bacon for free. Ive been coming for years, whereas you started working here just last month\n";
+    this->error();
 }
 
 void    Harl::error(void)
@@ -31,19 +34,29 @@ void    Harl::error(void)
 
 void    Harl::complain(std::string level)
 {
-    void	(Harl::*funcPtr)(void) = NULL;
-    void	(Harl::*funcArray[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
     std::string funclevel[4] = {"debug", "info", "warning", "error"};
     int i = -1;
     while (++i < 4)
     {
         if (funclevel[i] == level)
-            funcPtr = funcArray[i];
+            break;
     }
-    if (funcPtr == NULL) {
+    switch (i)
+    {
+    case 0:
+        this->debug();
+        break;
+    case 1:
+        this->info();
+        break;
+    case 2:
+        this->warning();
+        break;
+    case 3:
+        this->error();
+        break;
+    default:
         std::cout << "Harl doesn't complain about this, he is a civilised person !" << std::endl;
-        return ;
+        break;
     }
-    (this->*funcPtr)();
-
 }
