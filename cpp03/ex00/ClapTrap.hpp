13686be8f0c83bdb0938df1d46a3ce4623aa5ed3
Remi <remi.richard@nect.fr>
2026-07-05 11:01:58 +0200

    cpp03 commence

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string name;
+	int HitPoint;
+	int Energy;
+	int Attack;
+public:
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+	ClapTrap(std::string name);
+	~ClapTrap();
+};
---
+++ cpp03/ex01/ClapTrap.cpp
@@ -0,0 +1,40 @@
+#include "ClapTrap.hpp"
+
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+	this->HitPoint -= amount;
+	std::cout << this->name << " took " << amount << " damage, outch !" << std::endl;
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (this->Energy <= 0 || this->HitPoint <= 0)
+	{
+		std::cout << this->name << " is kapput :<" << std::endl;
+		return ;
+	}
+	this->Energy--;
+	std::cout << "ClapTrap " << this->name << " attacked " << target << " causing him to take " << this->Attack << " point of damage !!" << std::endl;
+}
+
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->Energy <= 0 || this->HitPoint <= 0)
+	{
+		std::cout << this->name << " is kapput :<" << std::endl;
+		return ;
+	}
+	this->Energy--;
+	std::cout << "ClapTrap " << this->name << " healed himself " << amount << " he is now at " << this->HitPoint << " HP !" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name):name(name), HitPoint(10), Energy(10), Attack(0)
+{
+	std::cout << "ClapTrap constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
